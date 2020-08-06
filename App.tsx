eb6ac50b8191a86ae4d6aa35204e95e8fa77c59e
Victor Silva <victormarques.ia@gmail.com>
2020-08-06 17:13:53 -0300

    Update App file

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
 import React from 'react';
 
+import { AppLoading } from 'expo';
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from '@expo-google-fonts/archivo';
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+import AppStack from './src/routes/AppStack';
+
 export default function App() {
-  return (
-    <>
-      <StatusBar style="light" />
-      <View />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <StatusBar style="light" />
+        <AppStack />
+      </>
+    );
+  }
 }
